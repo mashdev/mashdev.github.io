1b43f52d37c691654710bcb74ea1fff18e0b3e4f
Paul Lopez <paul.lopez@edgenuity.com>
2019-10-09 14:16:06 -0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="banner">
+      <Landing msg="mashdev" />
+    </div>
+    <div class="about">
+      <p>About</p>
+    </div>
+    <div class="projects">
+      <p> Projects </p>
+    </div>
+    <div class="contact">
+      <p> Contact </p>
+    </div>
+  </div>
+</template>
+
+<script>
+/* @ is an alias to /src */
+import Landing from "@/components/Landing.vue";
+
+export default {
+  name: "landing",
+  components: {
+    Landing
+  } 
+};
+</script>
